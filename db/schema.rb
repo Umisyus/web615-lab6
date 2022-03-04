39c27238742d8deb7f70568af4437f6d4a1c36b8
= <=>
2022-03-04 03:09:29 -0500

    edit database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,16 @@
 
 ActiveRecord::Schema.define(version: 20220304015609) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.text "title"
     t.text "content"
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
@@ -28,7 +31,7 @@ ActiveRecord::Schema.define(version: 20220304015609) do
     t.integer "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
@@ -44,4 +47,6 @@ ActiveRecord::Schema.define(version: 20220304015609) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "users"
+  add_foreign_key "comments", "users"
 end
