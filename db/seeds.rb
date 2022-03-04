a374ba990e0dabe355269771c86642cf47a1a0e7
= <=>
2022-03-04 01:56:22 -0500

    Modified seeds.rb to create and associate a user with comments and articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,32 @@
 for i in 1..100
-  @article = Article.new
-  @article.title = "Will #{Faker::Company.name} really #{Faker::Company.bs}?"
-  paragraph_1 = Faker::Lorem.paragraphs.join(' ')
-  paragraph_2 = Faker::Books::Lovecraft.paragraphs.join(' ')
-  paragraph_3 = Faker::Hipster.paragraphs.join(' ')
-  @article.content = "#{paragraph_1} <br /> #{paragraph_2} <br /> #{paragraph_3}"
-  if @article.save
-    p "#{@article.title} has been saved"
-    for ii in 1..10
-      @comment = Comment.new
-      @comment.article = @article
-      @comment.message = Faker::Hacker.say_something_smart
-      if @comment.save
-        p "Comment #{ii} has been saved for article #{@article.title}"
-      else
-        p @comment.errors
+  @user = User.new
+  @user.email = Faker::Internet.email
+  @user.password = "password"
+  if @user.save
+    p "#{@user.email} has been saved"
+    @article = Article.new
+    @article.title = "Will #{Faker::Company.name} really #{Faker::Company.bs}?"
+    paragraph_1 = Faker::Lorem.paragraphs.join(' ')
+    paragraph_2 = Faker::Books::Lovecraft.paragraphs.join(' ')
+    paragraph_3 = Faker::Hipster.paragraphs.join(' ')
+    @article.content = "#{paragraph_1} <br /> #{paragraph_2} <br /> #{paragraph_3}"
+    @article.user = @user
+    if @article.save
+      p "#{@article.title} has been saved"
+      for ii in 1..10
+        @comment = Comment.new
+        @comment.article = @article
+        @comment.message = Faker::Hacker.say_something_smart
+        @comment.user = @user
+        if @comment.save
+          p "Comment #{ii} has been saved for article #{@article.title}"
+        else
+          p @comment.errors
+        end
       end
+    else
+      p @article.errors
     end
-  else
-    p @article.errors
   end
+  p @user.errors
 end
